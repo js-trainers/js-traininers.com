5c466ac472bcb8afd30554db8baa176678e979ed
Brad Westfall <brad@azpixels.com>
2016-08-31 15:24:10 -0700

    gulp server

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
+var serve = require('gulp-serve');
+
+gulp.task('server', serve({
+  root: ['./public'],
+  port: 8080
+}));
 
 gulp.task('deploy', function() {
   return gulp.src('./public/**/*')
     .pipe(ghPages());
 });
+
+gulp.task('default', ['server'])
